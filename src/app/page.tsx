9697274bf466fa98af30b7d96fe4045ce094eae0
ChrisIBill <chrisibillingham1@gmail.com>
2023-10-13 20:58:43 -0500

    app page cleanup and fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import makeItRain from './make-it-rain.tsx'
+import makeItRain from './make-it-rain'
 
 export default function Home() {
     return (
@@ -9,7 +9,6 @@ export default function Home() {
                 <h1>Drizzle</h1>
                 <h3>(Yet Another Weather App)</h3>
             </header>
-            <Background />
         </main>
     )
 }
