d6382f871392c5724bd1450d86e3412f14b8242f
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2023-10-19 19:01:25 -0500

    Fixing state loading for fetched weather data on client side, introduced open-meteo as new weather api

--- src/app/weather/actions.ts
+++ src/app/weather/actions.ts
@@ -2,22 +2,29 @@
 
 import { CoordinatesType } from '@/lib/interfaces'
 
-const WeatherAPISrc = '/FarTestWeatherData.json'
+const WeatherAPISrc = '/SampleWeatherData.json'
 
 export async function getWeather(coords: CoordinatesType) {
-    let weatherData
-    return fetch(process.env.URL + WeatherAPISrc)
-        .then((res) => res.json())
-        .then(
-            (result) => {
-                console.log('Weather data fetched')
-                console.log(result)
-                result
-            },
-            (error) => {
-                console.log(error.message)
-                console.log('Error with fetching weather data')
-                error
-            }
-        )
+    const result = await fetch(process.env.URL + WeatherAPISrc)
+
+    if (!result.ok) {
+        console.error(result.status)
+        throw new Error('Error with fetching weather data')
+    }
+
+    return result.json()
+    //    return fetch(process.env.URL + WeatherAPISrc)
+    //        .then((res) => res.json())
+    //        .then(
+    //            (result) => {
+    //                console.log('Weather data fetched')
+    //                console.log(result)
+    //                result
+    //            },
+    //            (error) => {
+    //                console.log(error.message)
+    //                console.log('Error with fetching weather data')
+    //                error
+    //            }
+    //        )
 }
--- src/app/weather/page.tsx
+++ src/app/weather/page.tsx
@@ -16,42 +16,24 @@ export default function Page({
 }) {
     const [userWeather, setUserWeather] = useState<any>()
 
+    //get user coordinates from search params
     const coords: CoordinatesType = {
-        latitude: searchParams.lat ? Number(searchParams.lat) : 0,
-        longitude: searchParams.lon ? Number(searchParams.lon) : 0,
+        latitude: searchParams.lat ? Number(searchParams.lat) : 404,
+        longitude: searchParams.lon ? Number(searchParams.lon) : 404,
     }
-    console.log(coords)
+    if (coords.latitude == 404 || coords.longitude == 404) {
+        throw new Error('No coordinates found')
+    } else console.log(coords)
 
-    if (userWeather) {
-        console.log(userWeather)
-        console.log('user weather is set')
+    //use server action to fetch weather data from API
+    if (!userWeather) {
+        getWeather(coords)
+            .then((value) => setUserWeather(value))
+            .then(() => console.log(userWeather))
     } else {
-        console.log('user weather is not set')
+        console.log('user weather already set')
+        console.log(userWeather)
     }
-
-    useEffect(() => {
-        let ignore = false
-        getWeather(coords).then((value) => setUserWeather(value))
-        if (userWeather) {
-            console.log(userWeather)
-            ignore = true
-        } else {
-            console.log('user weather is not set')
-        }
-    }, [userWeather, coords])
-
-    //    const weatherData = handleWeatherData(coords)
-    //        .then((value) => {
-    //            console.log(value)
-    //            console.log('setting user weather')
-    //        })
-    //        .then((value) => value)
-    //    if (weatherData) {
-    //        console.log(weatherData)
-    //        console.log('user weather is set')
-    //    } else {
-    //        console.log('user weather is not set')
-    //    }
 
     return (
         <div className={styles.weatherPage}>
