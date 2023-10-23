090a93a56c9e38cc26a80760908a73930d158aad
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2023-10-22 19:31:32 -0500

    api wrangling and interfaces work

--- src/lib/forecast-shaper.ts
+++ src/lib/forecast-shaper.ts
@@ -1,17 +1,75 @@
-import { DailyWeatherCardType } from "./interfaces"
+import { DailyWeatherCardType, HourlyWeatherDataType } from './interfaces'
 
-export function forecastFormater(weatherApiData) {
-    const metadata = {
-        latitude: weatherApiData.latitude,
-        longitude: weatherApiData.longitude,
-        timezone: weatherApiData.timezone,
-        elevation: weatherApiData.elevation,
+function getApiMetadata(weatherApiData: any) {
+    try {
+        return {
+            latitude: weatherApiData.latitude,
+            longitude: weatherApiData.longitude,
+            timezone: weatherApiData.timezone,
+            elevation: weatherApiData.elevation,
+        }
+    } catch (e) {
+        throw new Error('Error getting metadata from weatherApiData')
     }
+}
+
+export function forecastFormater(weatherApiData: any) {
+    //TODO: add error handling
+    const metadata = getApiMetadata(weatherApiData)
     const current_weather = {
         units: weatherApiData.current_units,
-        weather: weatherApiData.current_weather,
+        weather: weatherApiData.current,
     }
-    const weather_forecast: DailyWeatherCardType[] = new Array(7).fill({})
 
+    const weather_forecast: DailyWeatherCardType[] = new Array(8).fill({})
+    const adf = weatherApiData.daily
+    const hdf = weatherApiData.hourly
+    const hdf_keys = weatherApiData.hourly.Object.keys()
+
+    function getHourlyWeather(
+        date: string,
+        index: number
+    ): HourlyWeatherDataType[] {
+        //Should be faster to search by index, but cant be sure hours match up so will need some form of check
+        //If check fails, then hours likely completely off, so either need to throw error or attempt to build
+        //hourly weather data from date string
+        const r = index * 24
+        const ret: Array<HourlyWeatherDataType> = new Array(24).map((_, i) => {
+            hdf_keys.forEach((key: string) => {
+                _[key] = hdf[key][r + i]
+            })
+            return _
+        })
+        return ret
+    }
+
+    weather_forecast.forEach((day, index) => {
+        day = {
+            daily_weather: {
+                date: adf.time[index],
+                weathercode: adf.weathercode[index],
+                temperature_2m_max: adf.temperature_2m_max[index],
+                temperature_2m_min: adf.temperature_2m_min[index],
+                apparent_temperature_max: adf.apparent_temperature_max[index],
+                apparent_temperature_min: adf.apparent_temperature_min[index],
+                sunrise: adf.sunrise[index],
+                sunset: adf.sunset[index],
+                uv_index_max: adf.uv_index_max[index],
+                precipitation_sum: adf.precipitation_sum[index],
+                rain_sum: adf.rain_sum[index],
+                showers_sum: adf.showers_sum[index],
+                snowfall_sum: adf.snowfall_sum[index],
+                precipitation_hours: adf.precipitation_hours[index],
+                precipitation_probability_max:
+                    adf.precipitation_probability_max[index],
+                windspeed_10m_max: adf.windspeed_10m_max[index],
+                windgusts_10m_max: adf.windgusts_10m_max[index],
+                winddirection_10m_dominant:
+                    adf.winddirection_10m_dominant[index],
+                hourly_weather: getHourlyWeather(adf.time[index], index),
+            },
+        }
+    })
+    return { metadata, current_weather, weather_forecast }
     //return {metadata, current_weather, weather_forecast[8]}
 }
--- src/lib/interfaces.tsx
+++ src/lib/interfaces.tsx
@@ -1,133 +1,43 @@
 export interface WeatherMetadata {
-    latitude: string;
-    longitude: string;
-    elevation: string;
-    generationTime: string;
-    timezone: string;
+    latitude: string
+    longitude: string
+    elevation: string
+    timezone: string
+    generationTime?: string
 }
 export interface CurrentWeatherReportType {
     units: {}
-    weather: {
-
-    }
+    weather: {}
+}
+export interface HourlyWeatherDataType {
+    [propName: string]: string
 }
 export interface DailyWeatherCardType {
     daily_weather?: {
-        weathercode: string;
-        temperature_2m_max: string;
-        temperature_2m_min: string;
-        apparent_temperature_max: string;
-        apparent_temperature_min: string;
-        sunrise: string;
-        sunset: string;
-        uv_index_max: string;
-        precipitation_sum: string;
-        rain_sum: string;
-        showers_sum: string;
-        snowfall_sum: string;
-        precipitation_hours: string;
-        precipitation_probability_max: string;
-        windspeed_10m_max: string;
-        windgusts_10m_max: string;
-        winddirection_10m_dominant: string;
-        hourly_weather: []
+        date: string
+        weathercode: string
+        temperature_2m_max: string
+        temperature_2m_min: string
+        apparent_temperature_max: string
+        apparent_temperature_min: string
+        sunrise: string
+        sunset: string
+        uv_index_max: string
+        precipitation_sum: string
+        rain_sum: string
+        showers_sum: string
+        snowfall_sum: string
+        precipitation_hours: string
+        precipitation_probability_max: string
+        windspeed_10m_max: string
+        windgusts_10m_max: string
+        winddirection_10m_dominant: string
+        hourly_weather: HourlyWeatherDataType[]
     }
 }
 export interface CoordinatesType {
     latitude: number
     longitude: number
-}
-export interface CurrentWeatherDataType {
-    dt: number
-    timezoneOffset: number
-}
-export interface MinutelyWeatherDataType {
-    dt: number
-    precipitation: number
-}
-export interface HourlyWeatherDataType {
-    dt: number
-    temp?: number
-    feels_like?: number
-    pressure?: number
-    humidity?: number
-    uvi?: number
-    clouds?: number
-    visibility?: number
-    wind_speed?: number
-    pop?: number
-    weather?: [
-        {
-            id?: number
-            main?: string
-            description?: string
-        },
-    ]
-}
-export interface DailyWeatherDataType {
-    dt: number
-    temp: {
-        day: number
-        min?: number
-        max?: number
-        night?: number
-        eve?: number
-        morn?: number
-    }
-    feels_like: {
-        day?: number
-        night?: number
-        eve?: number
-        morn?: number
-    }
-    pressure: number | undefined
-    humidity: number | undefined
-    dew_point: number | undefined
-    wind_speed: number | undefined
-    wind_deg: number | undefined
-    wind_gust: number | undefined
-    clouds: number | undefined
-    pop: number | undefined
-    rain?: number | undefined
-    snow?: number | undefined
-    uvi: number | undefined
-    weather?: [
-        {
-            id: number
-            main?: string
-            description?: string
-        },
-    ]
-}
-export interface WeatherAlertDataType {
-    //IDK What to use as key
-    sender_name?: string
-    event?: string
-    start?: number
-    end?: number
-    description?: string
-    tags?: any[]
-}
-export interface WeatherDataType {
-    current: CurrentWeatherDataType
-    minutely: MinutelyWeatherDataType[]
-    hourly: HourlyWeatherDataType[]
-    daily: DailyWeatherDataType[]
-    alerts?: WeatherAlertDataType[]
-}
-
-export interface ReportInfo {
-    label: string
-    info: string
-}
-export interface WeatherReportDataType {
-    time: string
-    dataset: ReportInfo[]
-}
-
-export interface ReportDisplayInfo {
-    //timeType: "hours" | "days",
-    labels: ''
 }
 
 export interface AreaChartDataType {
@@ -145,48 +55,139 @@ export interface AreaChartHandlerProps {
     labelDatasets: Array<string>
     title?: string
 }
-export interface DayTemperatures {
-    high: string
-    low: string
-    avg: string
-    morn: string[]
-    day: string[]
-    eve: string[]
-    night: string[]
-}
-export interface DayMiscWeather {
-    pressure: string
-    humidity: string
-    dew_point: string
-    wind_speed: string
-    wind_deg: string
-    wind_gust: string
-    clouds: string
-    pop: string
-    rain?: string
-    snow?: string
-    uvi: string
-}
-export interface DayWeatherInfo {
-    id: string
-    main: string | undefined
-    description: string
-    img_src: string
-}
-export interface DayWeatherData {
-    dt: number
-    data: {
-        temps: DayTemperatures
-        misc: DayMiscWeather
-        weather: DayWeatherInfo
-    }
-}
-
-export interface CurrentWeatherData {
-    dt: string
-    data: {
-        temp: number
-        feels: number
-    }
-}
-export interface SanitizedWeekWeatherData { }
+// export interface DayTemperatures {
+//     high: string
+//     low: string
+//     avg: string
+//     morn: string[]
+//     day: string[]
+//     eve: string[]
+//     night: string[]
+// }
+// export interface DayMiscWeather {
+//     pressure: string
+//     humidity: string
+//     dew_point: string
+//     wind_speed: string
+//     wind_deg: string
+//     wind_gust: string
+//     clouds: string
+//     pop: string
+//     rain?: string
+//     snow?: string
+//     uvi: string
+// }
+// export interface DayWeatherInfo {
+//     id: string
+//     main: string | undefined
+//     description: string
+//     img_src: string
+// }
+// export interface DayWeatherData {
+//     dt: number
+//     data: {
+//         temps: DayTemperatures
+//         misc: DayMiscWeather
+//         weather: DayWeatherInfo
+//     }
+// }
+//
+// export interface CurrentWeatherData {
+//     dt: string
+//     data: {
+//         temp: number
+//         feels: number
+//     }
+// }
+// export interface CurrentWeatherDataType {
+//     dt: number
+//     timezoneOffset: number
+// }
+// export interface MinutelyWeatherDataType {
+//     dt: number
+//     precipitation: number
+// }
+// export interface HourlyWeatherDataType {
+//     dt: number
+//     temp?: number
+//     feels_like?: number
+//     pressure?: number
+//     humidity?: number
+//     uvi?: number
+//     clouds?: number
+//     visibility?: number
+//     wind_speed?: number
+//     pop?: number
+//     weather?: [
+//         {
+//             id?: number
+//             main?: string
+//             description?: string
+//         },
+//     ]
+// }
+// export interface DailyWeatherDataType {
+//     dt: number
+//     temp: {
+//         day: number
+//         min?: number
+//         max?: number
+//         night?: number
+//         eve?: number
+//         morn?: number
+//     }
+//     feels_like: {
+//         day?: number
+//         night?: number
+//         eve?: number
+//         morn?: number
+//     }
+//     pressure: number | undefined
+//     humidity: number | undefined
+//     dew_point: number | undefined
+//     wind_speed: number | undefined
+//     wind_deg: number | undefined
+//     wind_gust: number | undefined
+//     clouds: number | undefined
+//     pop: number | undefined
+//     rain?: number | undefined
+//     snow?: number | undefined
+//     uvi: number | undefined
+//     weather?: [
+//         {
+//             id: number
+//             main?: string
+//             description?: string
+//         },
+//     ]
+// }
+// export interface WeatherAlertDataType {
+//     //IDK What to use as key
+//     sender_name?: string
+//     event?: string
+//     start?: number
+//     end?: number
+//     description?: string
+//     tags?: any[]
+// }
+// export interface WeatherDataType {
+//     current: CurrentWeatherDataType
+//     minutely: MinutelyWeatherDataType[]
+//     hourly: HourlyWeatherDataType[]
+//     daily: DailyWeatherDataType[]
+//     alerts?: WeatherAlertDataType[]
+// }
+//
+// export interface ReportInfo {
+//     label: string
+//     info: string
+// }
+// export interface WeatherReportDataType {
+//     time: string
+//     dataset: ReportInfo[]
+// }
+//
+// export interface ReportDisplayInfo {
+//     //timeType: "hours" | "days",
+//     labels: ''
+// }
