f61935ec96aff2643c16b77197bc9cee10b5fa88
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2023-10-24 01:31:21 -0500

    switched to genuine weather api

--- src/app/weather/actions.ts
+++ src/app/weather/actions.ts
@@ -4,11 +4,18 @@ import { forecastFormater } from '@/lib/forecast-shaper'
 import { CoordinatesType } from '@/lib/interfaces'
 
 const WeatherAPISrc = '/SampleWeatherData.json'
+const DEFAULT_WEATHER_PARAMS =
+    '&current=temperature_2m,relativehumidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weathercode,cloudcover,pressure_msl,surface_pressure,windspeed_10m,winddirection_10m&hourly=temperature_2m,relativehumidity_2m,dewpoint_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,weathercode,pressure_msl,surface_pressure,cloudcover,visibility,windspeed_10m,winddirection_10m,windgusts_10m,temperature_80m,uv_index&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,uv_index_max,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,precipitation_probability_max,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant&timezone=America%2FChicago'
 
 export async function getWeather(coords: CoordinatesType) {
     //TODO: cache handling
-    const result = await fetch(process.env.URL + WeatherAPISrc, {
-        cache: 'no-store',
+    const reqURL =
+        process.env.OPEN_METEO_API_URL +
+        `?latitude=${coords.latitude}&longitude=${coords.longitude}` +
+        DEFAULT_WEATHER_PARAMS
+    console.log('reqURL: ', reqURL)
+    const result = await fetch(reqURL, {
+        next: { revalidate: 3600 },
     })
 
     if (!result.ok) {
