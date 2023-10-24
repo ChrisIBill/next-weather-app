0a28564b7b7f983d48f0620d537fabc438fd99b1
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2023-10-24 00:49:23 -0500

    logging work

--- src/app/components/dayNightColorLayer.tsx
+++ src/app/components/dayNightColorLayer.tsx
@@ -29,16 +29,16 @@ function calcDayNightColorGradient(
     //calculates the percentage of the day that has passed, and returns a number between 6 and 18
     //or if before sunrise or after sunset, returns a number between 0 and 6 or 18 and 24
     if (curTime < sunrise) {
-        console.log('is before sunrise')
+        console.info('Good Morning')
         const timeBeforeSunrise = sunrise - curTime
         return Math.round((timeBeforeSunrise / dayLength) * 6)
     } else if (curTime > sunset) {
-        console.log('is after sunset')
+        console.info('Good Evening')
         const timeAfterSunset = curTime - sunset
         return Math.round((timeAfterSunset / dayLength) * 6) + 18
     } else {
-        //is daytime
-        console.log('is daytime')
+        //should be daytime
+        console.info('Good Day')
         const timeSinceSunrise = curTime - sunrise
         return Math.round((timeSinceSunrise / dayLength) * 12) + 6
     }
@@ -52,15 +52,11 @@ export const DayNightColorLayer: React.FC<ColorLayerProps> = ({
     const curTimeMinutes = clockTimeToMinutes(time)
     const sunriseMinutes = sunrise ? clockTimeToMinutes(sunrise) : 360
     const sunsetMinutes = sunset ? clockTimeToMinutes(sunset) : 1080
-    const [shortTime, date] = time.split('T')
-    console.log('sunrise: ', sunrise)
-    console.log('sunset: ', sunset)
 
     const dayLength =
         sunrise && sunset
             ? dayLengthCalculator(sunriseMinutes, sunsetMinutes)
-            : 600
-    console.log('Length of day: ', dayLength)
+            : 720
 
     const gradientHour = calcDayNightColorGradient(
         curTimeMinutes,
@@ -69,7 +65,6 @@ export const DayNightColorLayer: React.FC<ColorLayerProps> = ({
         dayLength
     )
     const dayNightColorStyle = `dayNightColorGradient${gradientHour}`
-    console.log('gradientHour: ', dayNightColorStyle)
     const className = styles[dayNightColorStyle + gradientHour]
     return (
         <div className={styles.wrapper}>
