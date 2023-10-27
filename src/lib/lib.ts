6a0aa521ce52a5fc2e7264e41bcd34b1884a944c
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2023-10-26 19:06:14 -0500

    improved bezier curve function moved to lib

--- src/lib/lib.ts
+++ src/lib/lib.ts
@@ -1,3 +1,5 @@
+import { PosCoordinates } from './interfaces'
+
 function displayTodayOrTomorrow() {
     const today = new Date()
     const tomorrow = new Date(today)
@@ -48,4 +50,36 @@ export const cancellablePromise = (promise: Promise<any | void>) => {
 export function clockTimeToMinutes(time: string) {
     const [hours, minutes] = time.split(':')
     return parseInt(hours) * 60 + parseInt(minutes)
+}
+
+/**
+ * @description given a point [0-1] and a set of constant control points, returns a point along
+ * the bezier curve
+ *
+ * @param {number} t - a point ranging from 0 to 1
+ * @param {PosCoordinates} p0 - {x: number, y: number} control point 1
+ * @param {PosCoordinates} p1 - {x: number, y: number} control point 2
+ * @param {PosCoordinates} p2 - {x: number, y: number} control point 3
+ * @param {PosCoordinates} p3 - {x: number, y: number} control point 4
+ * @returns {PosCoordinates} - {x: number, y: number} point along the bezier curve
+ */
+export function bezierCurve(
+    t: number,
+    p0: PosCoordinates,
+    p1: PosCoordinates,
+    p2: PosCoordinates,
+    p3: PosCoordinates
+): PosCoordinates {
+    const cX = 3 * (p1.x - p0.x),
+        bX = 3 * (p2.x - p1.x) - cX,
+        aX = p3.x - p0.x - cX - bX
+
+    const cY = 3 * (p1.y - p0.y),
+        bY = 3 * (p2.y - p1.y) - cY,
+        aY = p3.y - p0.y - cY - bY
+
+    const x = aX * Math.pow(t, 3) + bX * Math.pow(t, 2) + cX * t + p0.x
+    const y = aY * Math.pow(t, 3) + bY * Math.pow(t, 2) + cY * t + p0.y
+
+    return { x: x, y: y }
 }
