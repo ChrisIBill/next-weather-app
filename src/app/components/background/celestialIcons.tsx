8c9573faf63b430223a1dff243a2c669c743c50a
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2023-10-26 19:09:17 -0500

    working celestial icon positioning along bezier curve

--- src/app/components/background/celestialIcons.tsx
+++ src/app/components/background/celestialIcons.tsx
@@ -1,24 +1,10 @@
 'use client'
 import './celestialIcons.css'
-import { Slider, SliderRail, SliderTrack, styled } from '@mui/material'
-import CircularSlider from '@fseehawer/react-circular-slider'
-import {
-    CircularInput,
-    CircularTrack,
-    CircularProgress,
-    CircularThumb,
-    useCircularInputContext,
-} from 'react-circular-input'
 import React from 'react'
 import styles from './celestialIcons.module.scss'
 import { MoonIcon, SunIcon } from '../icons'
-import { constants } from 'perf_hooks'
 import { PosCoordinates } from '@/lib/interfaces'
-//import dynamic from 'next/dynamic'
-//const DynamicMoon = dynamic(() =>
-//    import('../icons').then((mod) => mod.MoonIcon)
-//)
-//const DynamicSun = dynamic(() => import('../icons').then((mod) => mod.SunIcon))
+import { bezierCurve } from '@/lib/lib'
 
 export interface CelestialIconsProps {
     isDay: boolean
@@ -28,89 +14,37 @@ export interface CelestialIconsProps {
     event?: string
 }
 
-const CustomComponent = () => {
-    const { getPointFromValue, value } = useCircularInputContext()
-    const coords = getPointFromValue()
-    const [x, y] = coords ? [coords.x, coords.y] : [0, 0]
-
-    return (
-        <text x={x} y={y} style={{ pointerEvents: 'none' }}>
-            <MoonIcon />
-        </text>
-    )
-}
-
-export const DepCelestialIconsHandler: React.FC<CelestialIconsProps> = ({
-    isDay,
-    timePercent,
-}: CelestialIconsProps) => {
-    const [value, setValue] = React.useState<number>(0)
-    const celestialRail = styled(SliderRail)(({ theme }) => ({
-        backgroundColor: 'white',
-    }))
-    return (
-        <div className={styles.wrapper}>
-            <CircularInput value={value} onChange={setValue}>
-                <CircularSlider trackColor="#fff" trackSize={10} width={700} />
-                <CircularThumb />
-                <CustomComponent />
-                {/* isDay ? (
-                    <SunIcon inputContext={useCircularInputContext} />
-                ) : (
-                    <MoonIcon inputContext={useCircularInputContext} />
-                )*/}
-            </CircularInput>
-        </div>
-    )
-}
-export function bezierCurve(
-    t: number,
-    p0: PosCoordinates,
-    p1: PosCoordinates,
-    p2: PosCoordinates,
-    p3: PosCoordinates
-): PosCoordinates {
-    const cX = 3 * (p1.x - p0.x),
-        bX = 3 * (p2.x - p1.x) - cX,
-        aX = p3.x - p0.x - cX - bX
-
-    const cY = 3 * (p1.y - p0.y),
-        bY = 3 * (p2.y - p1.y) - cY,
-        aY = p3.y - p0.y - cY - bY
-
-    const x = aX * Math.pow(t, 3) + bX * Math.pow(t, 2) + cX * t + p0.x
-    const y = aY * Math.pow(t, 3) + bY * Math.pow(t, 2) + cY * t + p0.y
-
-    return { x: x, y: y }
-}
 export const CelestialIconsHandler: React.FC<CelestialIconsProps> = ({
     isDay,
     timePercent,
 }: CelestialIconsProps) => {
+    //TODO: Should probably handle client scaling higher up the tree
     const { innerWidth: width, innerHeight: height } = window
-    console.log('width', width)
-    console.log('height', height)
-    const widthStretchFator = width / 400
-    const heightStretchFator = height / 400
+    const xScale = width / 300
+    const yScale = height / 300
+    const avgScale = (xScale + yScale) / 2
+    const [iconScaledX, iconScaledY] = [25 * avgScale, 25 * avgScale]
+    console.log('Client Window properties: ', {
+        width,
+        height,
+        xScale,
+        yScale,
+        avgScale,
+    })
     const [p0, p1, p2, p3] = [
-        { x: 30 * widthStretchFator, y: 400 * heightStretchFator },
-        { x: 150 * widthStretchFator, y: 300 * heightStretchFator },
-        { x: 270 * widthStretchFator, y: 300 * heightStretchFator },
-        { x: 390 * widthStretchFator, y: 400 * heightStretchFator },
+        { x: 0 * xScale, y: 110 * yScale },
+        { x: 100 * xScale, y: 0 * yScale },
+        { x: 200 * xScale, y: 0 * yScale },
+        { x: 300 * xScale, y: 110 * yScale },
     ]
-    const celestialRail = styled(SliderRail)(({ theme }) => ({
-        backgroundColor: 'white',
-    }))
     const bezierPos = bezierCurve(timePercent, p0, p1, p2, p3)
-    const timePercents = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
-    const beziers: PosCoordinates[] = timePercents.map((timePercent) =>
-        bezierCurve(timePercent, p0, p1, p2, p3)
-    )
-
-    console.log('beziers: ', beziers)
+    let bezierPath = ''
+    for (let i = 0; i < 1.01; i += 0.01) {
+        const { x, y } = bezierCurve(i, p0, p1, p2, p3)
+        bezierPath += `L${x} ${y}`
+    }
     console.log('timepercent', timePercent)
-    const [iconCoords, setIconCoords] = React.useState({ x: 10, y: 80 })
-
+    console.log('x, y, avg scale factors: ', xScale, yScale, avgScale)
     return (
         <svg
             className={styles.CelestialsSVG}
@@ -119,15 +53,19 @@ export const CelestialIconsHandler: React.FC<CelestialIconsProps> = ({
             version="1.1"
             xmlns="http://www.w3.org/2000/svg"
         >
-            <circle
-                className="knob"
-                r="25"
-                cx={bezierPos.x}
-                cy={bezierPos.y}
-                fill="#88CE02"
-                stroke-width="4"
-                stroke="#111"
-            />
+            {/*
+                ALLOWS FOR TESTING OF BEZIER CURVE 
+                <path d={`M${p0.x} ${p0.y} ${bezierPath}`} stroke="#111" />
+            */}
+            <foreignObject
+                className={styles.svgIconObject}
+                height={iconScaledY}
+                width={iconScaledX}
+                x={bezierPos.x}
+                y={bezierPos.y}
+            >
+                <MoonIcon />
+            </foreignObject>
         </svg>
     )
 }
--- src/app/components/icons.tsx
+++ src/app/components/icons.tsx
@@ -1,25 +1,43 @@
 import React from 'react'
 import styles from './icons.module.css'
 import { useCircularInputContext } from 'react-circular-input'
+import { PosCoordinates } from '@/lib/interfaces'
 
-export interface MoonIconProps {
-    inputContext?: any
+export interface IconProps {
+    pos?: PosCoordinates
+    xScale?: number
+    yScale?: number
+    width?: number
+    height?: number
+}
+export interface MoonIconProps extends IconProps {
     phase?: number
     eclipse?: boolean
+    condition?: string
 }
 export const MoonIcon: React.FC<MoonIconProps> = ({
-    inputContext,
+    pos,
+    xScale,
+    yScale,
+    width,
+    height,
     phase,
+    condition,
 }: MoonIconProps) => {
-    return <div className={styles.moon}>Moon</div>
+    return <div className={styles.moon}></div>
 }
-export interface SunIconProps {
-    inputContext?: any
+export interface SunIconProps extends IconProps {
     eclipse?: boolean
+    condition?: string
 }
 export const SunIcon: React.FC<SunIconProps> = ({
-    inputContext,
+    pos,
+    xScale,
+    yScale,
+    width,
+    height,
     eclipse,
+    condition,
 }: SunIconProps) => {
     return <div className={styles.sunIcon}>Sun</div>
 }
