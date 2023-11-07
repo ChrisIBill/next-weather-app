af0e834c1ad6fd50f2ceae313d47b106e8d4faff
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2023-11-06 20:34:41 -0600

    swapped color layer css for sass and implemented starry night sky

--- src/app/components/background/dayNightColorLayer.tsx
+++ src/app/components/background/dayNightColorLayer.tsx
@@ -1,6 +1,6 @@
 //NOTE: Current implementation will struggle with unusual sunrise/sunset times
 //      as might appear in the far north or south.  For example, in Tromso, Norway,
-import styles from './dayNightColorLayer.module.css'
+import styles from './dayNightColorLayer.module.scss'
 import { percentToGradientStringMapper } from '@/lib/time'
 
 export interface ColorLayerProps {
@@ -13,6 +13,7 @@ export const DayNightColorLayer: React.FC<ColorLayerProps> = ({
     timePercent,
 }: ColorLayerProps) => {
     //minutes in day = 1440
+    console.log('DayNightColorLayer: ', isDay, timePercent)
 
     const gradientHour = percentToGradientStringMapper(isDay, timePercent)
     const dayNightColorStyle = `dayNightColorGradient${gradientHour}`
@@ -20,6 +21,40 @@ export const DayNightColorLayer: React.FC<ColorLayerProps> = ({
         <div
             id={styles.dayNightColorLayer}
             className={styles[dayNightColorStyle]}
-        ></div>
+        >
+            {isDay ? <></> : <StarryNightBackground />}
+        </div>
+    )
+}
+
+export const StarryNightBackground: React.FC = () => {
+    return (
+        <div className={styles.starsWrapper}>
+            <Stars num={200} />
+        </div>
+    )
+}
+
+export interface StarsProps {
+    num: number
+}
+export const Stars: React.FC<StarsProps> = (props: StarsProps) => {
+    return (
+        <svg className={styles.stars}>
+            {[...Array(props.num)].map((e, i) => {
+                const cx = Math.round(Math.random() * 10000) / 100 + '%'
+                const cy = Math.round(Math.random() * 10000) / 100 + '%'
+                const r = Math.round(Math.random() * 10) / 10
+                return (
+                    <circle
+                        key={i}
+                        className={styles.star}
+                        cx={cx}
+                        cy={cy}
+                        r={r}
+                    />
+                )
+            })}
+        </svg>
     )
 }
