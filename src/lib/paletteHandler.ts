a8c006f634a27ec500d610df04717ac4b4786c3e
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2023-11-09 22:20:35 -0600

    changed function to default export

---
+++ src/lib/paletteHandler.ts
@@ -0,0 +1,23 @@
+import palette from './export.module.scss'
+export default function paletteHandler() {
+    if (typeof window === 'undefined')
+        throw new Error(
+            'Client-side function paletteHandler() running on server-side'
+        )
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        return {
+            background: palette.darkBackground,
+            secondary: palette.darkSecondary,
+            textPrimary: palette.darkTextPrimary,
+            textSecondary: palette.darkTextSecondary,
+            accent: palette.darkAccent,
+        }
+    }
+    return {
+        background: palette.lightBackground,
+        secondary: palette.lightSecondary,
+        textPrimary: palette.lightTextPrimary,
+        textSecondary: palette.lightTextSecondary,
+        accent: palette.lightAccent,
+    }
+}
