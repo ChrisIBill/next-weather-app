d605d850dceffa082e016e8d8a2c63671b8be7b5
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2023-11-16 01:40:36 -0600

    simple clouds generation work

--- src/app/components/background/clouds.tsx
+++ src/app/components/background/clouds.tsx
@@ -1,19 +1,52 @@
 import React from 'react'
 import styles from './clouds.module.scss'
+import { useTheme } from '@/lib/context'
+import paletteHandler from '@/lib/paletteHandler'
 export interface CloudsProps {
     cloudCover: number
+    theme?: string
 }
-export const Clouds: React.FC<CloudsProps> = () => {
+export const Clouds: React.FC<CloudsProps> = (props: CloudsProps) => {
+    const theme = useTheme()
+    const CloudLayer1 = () => {
+        const lengths = []
+        const delays = []
+        const runtime = Math.random() * 15 + 20
+        for (let i = 0; i < 10; i++) {
+            lengths.push(Math.random() * 5 + 7)
+            delays.push(Math.random() * 5 + 3)
+        }
+    }
     return (
         <div className={styles.cloudsWrapper}>
+            <CloudLayerGenerator
+                cloudCover={props.cloudCover}
+                theme={theme.theme}
+            />
+        </div>
+    )
+}
+
+const CloudLayerGenerator = (props: CloudsProps) => {
+    const NUM_CLOUDS = 10
+    const animationTime = Math.random() * 15 + 20
+    const clouds = Array(NUM_CLOUDS)
+        .fill(undefined)
+        .map((cloud, index) => (
             <div
+                key={`cloud-${index}`}
                 className={styles.cloud}
+                data-theme={props.theme}
                 style={
                     {
-                        '--width': '12rem',
+                        '--cloudWidth': `${Math.random() * 5 + 7}rem`,
+                        '--cloudHeight': `${3.5 - index / 10}rem`,
+                        '--delay': `${Math.random() * 17 + 3}s`,
+                        '--animationSpeed': `${animationTime}s`,
+                        '--zIndex': `${index + 5}`,
                     } as React.CSSProperties
                 }
             ></div>
-        </div>
-    )
+        ))
+    return <ul className={styles.cloudsLayer}>{clouds}</ul>
 }
