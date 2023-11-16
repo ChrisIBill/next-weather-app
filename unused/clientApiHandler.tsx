d7cb410bb3eaca03bc962d24e502127f120ef0bc
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2023-11-15 20:18:31 -0600

    moved unimplemented client side api handler to new folder

---
+++ unused/clientApiHandler.tsx
@@ -0,0 +1,38 @@
+import {
+    WeatherApiResponseType,
+    WeatherMetadata,
+    WeatherForecastType,
+    DailyWeatherForecastType,
+} from '@/lib/interfaces'
+import TemperatureClass, { ApiTempUnitStrings } from '@/lib/temperature'
+
+function objectDeepFormatter(obj: any, func: (k: string, v: any) => any) {
+    for (const key in obj) {
+        if (typeof obj[key] == 'object')
+            obj[key] = objectDeepFormatter(obj[key], func)
+        else obj[key] = func(key, obj[key])
+    }
+    return obj
+}
+
+export function weatherApiHandler(value: WeatherApiResponseType) {
+    //Wraps convertable values in their respective classes
+    const metadata: WeatherMetadata = value.metadata
+    const forecast: WeatherForecastType = Array(8).fill({})
+
+    function weatherApiFormatter(key: string, elem: string | number) {
+        if (key.includes('temperature')) {
+            const temp: number = +elem
+            return new TemperatureClass(
+                temp,
+                metadata.units.all[key] as ApiTempUnitStrings
+            )
+        } else return elem
+    }
+
+    value.forecast.map((day: DailyWeatherForecastType, index) => {
+        forecast[index] = objectDeepFormatter(day, weatherApiFormatter)
+    })
+    console.log('Client forecast: ', forecast)
+    return { forecast, metadata }
+}
