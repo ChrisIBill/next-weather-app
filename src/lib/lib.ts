e93aa97cbe2f1d7a9990527c84e4f7227d687ccb
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2023-11-15 20:17:40 -0600

    lib function to flatten an object to single depth

--- src/lib/lib.ts
+++ src/lib/lib.ts
@@ -129,3 +129,31 @@ export const stringLiteralGenerator = function* (
 export function formatUserPrefs(str: string): string {
     return str.toLowerCase().replace(/[^a-z]/gi, '')
 }
+
+/**
+ * Flatten a multidimensional object
+ *
+ * For example:
+ *   flattenObject{ a: 1, b: { c: 2 } }
+ * Returns:
+ *   { a: 1, c: 2}
+ */
+export const flattenObject = (obj: any) => {
+    const flattened = {}
+
+    Object.keys(obj).forEach((key) => {
+        const value = obj[key]
+
+        if (
+            typeof value === 'object' &&
+            value !== null &&
+            !Array.isArray(value)
+        ) {
+            Object.assign(flattened, flattenObject(value))
+        } else {
+            flattened[key] = value
+        }
+    })
+
+    return flattened
+}
