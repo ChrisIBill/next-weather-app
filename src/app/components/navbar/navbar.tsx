7c08ea03d549bc0393f992fb3c1e4e10f89335fa
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2023-11-28 03:36:04 -0600

    navbar handling of mui theme and scaling fixes for mobile

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -1,43 +1,54 @@
 'use client'
-import { AppBar, Autocomplete, Toolbar, styled } from '@mui/material'
+import { AppBar, Toolbar, styled } from '@mui/material'
 import styles from './navbar.module.scss'
 import SearchBar from './search-bar'
 import { Settings } from './settings'
-import paletteHandler from '@/lib/paletteHandler'
-import UserPrefs from '@/lib/user'
 import React from 'react'
-import useLocalStorage from 'use-local-storage'
-import { useTheme } from '@/lib/context'
+import { useTheme } from '@mui/material/styles'
+import { useWindowDimensions } from '@/lib/hooks'
+
+const TitleWrapper = styled('div')(({ theme }) => ({
+    [theme.breakpoints.down('sm')]: {
+        h3: {
+            display: 'none',
+        },
+    },
+}))
 
 export interface NavBarProps {}
 const NavBar: React.FC<NavBarProps> = () => {
     //TODO: If mobile, render navbar in footer
     const theme = useTheme()
-    const palette = paletteHandler(theme.theme)
-    console.log('User Theme: ', theme)
+    const palette = theme.palette
+    const windowDimensions = useWindowDimensions()
     return (
-        <header className={styles.header} data-theme={theme.theme}>
+        <header className={styles.header} data-theme={theme.palette.mode}>
             <div className={styles.glowBox} />
             <AppBar
                 className={styles.MuiAppBar}
-                data-theme={theme.theme}
+                data-theme={theme.palette.mode}
                 sx={{}}
             >
                 <Toolbar
                     className={styles.Toolbar}
                     sx={{
-                        background: `${palette.primary}`,
+                        background: palette.primary.main,
+                        color: palette.primary.contrastText,
                         borderRadius: '0 0 1rem 1rem',
-                        color: palette.textPrimary,
+                        [theme.breakpoints.down('md')]: {
+                            justifyContent: 'center',
+                        },
                     }}
                 >
-                    <div className={styles.fillerElement}></div>
-                    <div className={styles.titleWrapper}>
+                    {windowDimensions.width > 900 ? (
+                        <div className={styles.fillerElement}></div>
+                    ) : null}
+                    <TitleWrapper className={styles.titleWrapper}>
                         <h1 className={styles.title}>Drizzle</h1>
                         <h3 className={styles.subtitle}>
                             (Yet Another Weather App)
                         </h3>
-                    </div>
+                    </TitleWrapper>
                     <div className={styles.itemsWrapper}>
                         <SearchBar />
                         <Settings />
