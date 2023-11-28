b014615bb97d4c702159e9069bbc53cb00708583
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2023-11-28 03:12:01 -0600

    init of mui theme handler to replace current implementation of theme handler

--- src/lib/context.tsx
+++ src/lib/context.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { createContext, useContext, useEffect, useState } from 'react'
+import { createContext, useContext, useEffect, useMemo, useState } from 'react'
 import UserPrefs, {
     DEFAULT_USER_PREFS,
     ThemeLiteralsType,
@@ -7,12 +7,24 @@ import UserPrefs, {
     UserPreferencesInterface,
 } from '@/lib/user'
 import { Tillana } from 'next/font/google'
+import { PaletteMode, Theme, createTheme } from '@mui/material'
+import { getPaletteMode } from './paletteHandler'
+import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles'
 
 type ThemeState = {
     theme: ThemeLiteralsType
     setTheme: (theme: ThemeLiteralsType) => void
 }
+type mThemeState = {
+    theme: Theme
+}
+type ColorState = {
+    toggleColorMode: () => void
+}
+
 const ThemeContext = createContext<ThemeState | null>(null)
+const MThemeContext = createContext<mThemeState | null>(null)
+const ColorModeContext = createContext<ColorState | null>(null)
 
 export const useTheme = () => {
     const context = useContext(ThemeContext)
@@ -22,6 +34,20 @@ export const useTheme = () => {
 
     return context
 }
+export const useMTheme = () => {
+    const context = useContext(MThemeContext)
+    if (!context) {
+        throw new Error('useMTheme must be used within a ThemeProvider')
+    }
+    return context
+}
+export const useColorMode = () => {
+    const context = useContext(ColorModeContext)
+    if (!context) {
+        throw new Error('useColorMode must be used within a ThemeProvider')
+    }
+    return context
+}
 
 export interface ThemeProviderProps {
     children: React.ReactNode
@@ -29,7 +55,7 @@ export interface ThemeProviderProps {
 
 const getInitialTheme = () => {
     let theme = 'light'
-    if (localStorage && localStorage.getItem('theme')) {
+    if (localStorage !== undefined) {
         theme = localStorage.getItem('theme') || 'light'
         theme = JSON.parse(theme)
     } else if (
@@ -42,18 +68,53 @@ const getInitialTheme = () => {
 
     return theme as ThemeLiteralsType
 }
+const getInitialMTheme = () => {
+    let theme = 'light'
+    if (localStorage !== undefined) {
+        theme = localStorage.getItem('theme') || 'light'
+        theme = JSON.parse(theme)
+    } else if (
+        window.matchMedia &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches
+    ) {
+        theme = 'dark'
+    }
+    document.documentElement.setAttribute('data-theme', theme)
+
+    return theme as PaletteMode
+}
 
 export const ThemeProvider = (props: ThemeProviderProps) => {
     const [theme, setTheme] = useState<ThemeLiteralsType>(getInitialTheme())
 
     useEffect(() => {
         localStorage.setItem('theme', JSON.stringify(theme))
+        console.log('Setting theme: ', theme)
         document.documentElement.setAttribute('data-theme', theme)
     })
     return (
         <ThemeContext.Provider value={{ theme, setTheme }}>
             {props.children}
         </ThemeContext.Provider>
+    )
+}
+
+export const MThemeProvider = (props: ThemeProviderProps) => {
+    const [mode, setMode] = useState<PaletteMode>(getInitialMTheme())
+    const colorMode = useMemo(
+        () => ({
+            toggleColorMode: () => {
+                setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'))
+            },
+        }),
+        []
+    )
+
+    const mTheme = useMemo(() => createTheme(getPaletteMode(mode)), [mode])
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <MuiThemeProvider theme={mTheme}>{props.children}</MuiThemeProvider>
+        </ColorModeContext.Provider>
     )
 }
 
--- src/lib/paletteHandler.ts
+++ src/lib/paletteHandler.ts
@@ -1,3 +1,4 @@
+import { PaletteMode } from '@mui/material'
 import palette from './export.module.scss'
 export default function paletteHandler(theme?: string) {
     if (theme === 'dark')
@@ -20,3 +21,47 @@ export default function paletteHandler(theme?: string) {
         accentSecondary: palette.lightAccentSecondary,
     }
 }
+
+export const getPaletteMode = (mode: PaletteMode) => ({
+    palette: {
+        mode,
+        contrastThreshold: 4.5,
+        ...(mode === 'light'
+            ? {
+                  primary: {
+                      main: palette.lightPrimary,
+                  },
+                  secondary: {
+                      main: palette.lightSecondary,
+                  },
+                  blue: {
+                      main: palette.lightBlue,
+                  },
+                  violet: {
+                      main: '#7c4dff',
+                  },
+                  textPrimary: palette.lightTextPrimary,
+                  textSecondary: palette.lightTextSecondary,
+                  textDarkPrimary: palette.darkTextPrimary,
+                  textDarkSecondary: palette.darkTextSecondary,
+              }
+            : {
+                  primary: {
+                      main: palette.darkPrimary,
+                  },
+                  secondary: {
+                      main: palette.darkSecondary,
+                  },
+                  blue: {
+                      main: palette.darkBlue,
+                  },
+                  violet: {
+                      main: palette.darkViolet,
+                  },
+                  textPrimary: palette.darkTextPrimary,
+                  textSecondary: palette.darkTextSecondary,
+                  textLightPrimary: palette.lightTextPrimary,
+                  textLightSecondary: palette.lightTextSecondary,
+              }),
+    },
+})
