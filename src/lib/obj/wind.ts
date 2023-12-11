8cc90418c6efc0c31e6e6298e680fde4b346bf3f
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2023-12-10 23:36:59 -0600

    adding const wind strings

--- src/lib/obj/wind.ts
+++ src/lib/obj/wind.ts
@@ -1,1 +1,2 @@
-export const WindUnitStrings = ['ms', 'kph', 'mph', 'kn'] as const
+export const WIND_UNIT_STRINGS = ['ms', 'kph', 'mph', 'kn'] as const
+export type WindUnitStringsType = (typeof WIND_UNIT_STRINGS)[number]
