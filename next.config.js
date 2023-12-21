bdb5ec5dd4e659db800ffc5a4b7cd115efe274b9
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2023-12-20 21:44:41 -0600

    whoops haha

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
 const nextConfig = {
-    output: 'export',
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
