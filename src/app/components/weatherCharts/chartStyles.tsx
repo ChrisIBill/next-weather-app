55684038a127ccadc903c9cc047b03191f7304e9
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2023-12-31 19:43:15 -0600

    new chart styles and consts.

---
+++ src/app/components/weatherCharts/chartStyles.tsx
@@ -0,0 +1,8 @@
+export const LegendWrapperStyle = {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    position: 'absolute',
+    borderRadius: '16px',
+    boxSizing: 'border-box',
+    background: 'rgba(0,0,0,0.3)',
+}
